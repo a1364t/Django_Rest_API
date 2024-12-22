0c4d74136ff34497c1158bb60091244f42cc0c87
Alireza <ali1985reza@gmail.com>
2024-12-22 23:10:09 +1100

    POST method

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,7 @@
 
 from decimal import Decimal
 from rest_framework import serializers
+from django.utils.text import slugify
 
 from .models import Category, Product
 
@@ -11,17 +12,29 @@ class CategorySerializer(serializers.Serializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    
     title = serializers.CharField(max_length=255, source='name') # what stored in db(as name)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_after_GST = serializers.SerializerMethodField(method_name='calculate_tax')
 
-    def calculate_tax(self, product):
-        return round(product.unit_price * Decimal(1.1), 2)
-    category = CategorySerializer()
     class Meta:
         model = Product
-        fields = ['id', 'title','price', 'price_after_GST', 'inventory', 'category']
+        fields = ['id', 'title','price', 'category', 'price_after_GST', 'inventory', 'slug']
+
+    def calculate_tax(self, product):
+        return round(Decimal(product.unit_price) * Decimal(1.1), 2)
+    # category = CategorySerializer()
+
+    def validate(self, data):
+        if len(data['name']) < 6:
+            raise serializers.ValidationError('Product title should be at least 6 character.')
+        return data
+    
+    def create(self, validated_data):
+        product = Product(**validated_data)
+        product.slug = slugify(product.name)
+        product.save()
+        return product
+ 
 #     category = serializers.HyperlinkedRelatedField(
 #          queryset=Category.objects.all(),
 #          view_name='category-detail',
--- store/views.py
+++ store/views.py
@@ -7,17 +7,29 @@ from rest_framework import status
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_lis(request):
-    products_queryset = Product.objects.select_related('category').all()
-    serializer = ProductSerializer(products_queryset, many=True, context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products_queryset = Product.objects.select_related('category').all()
+        serializer = ProductSerializer(products_queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response('Everything is ok')
 
 @api_view()
 def product_detail(request, pk):
     product = get_object_or_404(Product.objects.select_related('category'), pk=pk)
     serializer= ProductSerializer(product, context={'request': request})
     return Response(serializer.data)
+    
+
+        # if serializer.is_valid():
+        #     serializer.validated_data
+        # else:
+        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view()
 def category_detail(request, pk):
